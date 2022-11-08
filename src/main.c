3ac963fbd79a0c2dd1b8b3bffd47606dbe006c4e
root <root@localhost>
2022-11-08 19:46:28 +0530

    auto publish 1667916988383

---
+++ src/main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <conio.h>
+int main ()
+{
+  int a=2,b =3,c;
+  clrscr();//used after declaration 
+  c=a+b;
+  printf(The sum of %d and %d is %d",a, b, c); 
+  getch()
+  return 0;
+}
